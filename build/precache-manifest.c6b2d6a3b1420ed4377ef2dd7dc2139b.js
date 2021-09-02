a232f7baeda3dcc176109dc2b7406967f8e40291
elliot drew <ed8962@googlemail.com>
2021-09-02 13:07:53 +0100

    upload build

---
+++ build/precache-manifest.c6b2d6a3b1420ed4377ef2dd7dc2139b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b764ccf4ca9ed35f726b59505b7c7ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ab8122639cf4dc0db96",
+    "url": "/static/css/main.4014da4f.chunk.css"
+  },
+  {
+    "revision": "18511f1459e10a72c1fe",
+    "url": "/static/js/2.d62f25b6.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.d62f25b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab8122639cf4dc0db96",
+    "url": "/static/js/main.e2675c93.chunk.js"
+  },
+  {
+    "revision": "86e69d7d0dea99d66291",
+    "url": "/static/js/runtime-main.18f97559.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
